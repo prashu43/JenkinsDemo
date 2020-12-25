28e9538734f55825cc5c0a8bb7e95ad3316af45a
prashu43 <prashant.namburu@gmail.com>
2020-12-25 23:53:17 +0530

    modified java class

--- src/uploadToGit.java
+++ src/uploadToGit.java
@@ -3,9 +3,10 @@ public class uploadToGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	for(int i = 0; i<10 ; i++){
 		
-		System.out.println("Jenkins Pipeline Setup");
-
+		System.out.println("Calling Jenkins Pipeline Setup" + i);
+	}
 	}
 
 }
